e6761c1717859ee2ad99b67b5dd992616cfd92a2
OneCrazyman <cjw7155@naver.com>
2023-08-09 14:59:51 +0900

    0809 1

---
+++ baekjoon/1546.py
@@ -0,0 +1,9 @@
+#평균
+
+import sys
+sys.stdin = open("C:/Users/kms/Desktop/git/AlgorithmStudy/baekjoon/input.txt") #제출시에는 해당라인 주석처리
+SIZE = int(input())
+for i in range(SIZE):
+    a = list(map(int,input().split()))
+    print(a)
+

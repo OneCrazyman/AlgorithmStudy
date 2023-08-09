f655d9dc7ab8d1beee733ebb8c799fadb7278d6e
OneCrazyman <cjw7155@naver.com>
2023-08-09 16:06:46 +0900

    0809 2

--- baekjoon/1546.py
+++ baekjoon/1546.py
@@ -1,9 +1,21 @@
 #평균
+def solution():
+    n=[]
+    for i in range(2):
+        n.append(input())
+    n[0] = int(n[0]) #개수
+    n[1] = list(map(int,n[1].split()))
 
-import sys
-sys.stdin = open("C:/Users/kms/Desktop/git/AlgorithmStudy/baekjoon/input.txt") #제출시에는 해당라인 주석처리
-SIZE = int(input())
-for i in range(SIZE):
-    a = list(map(int,input().split()))
-    print(a)
+    #최고점
+    _max = n[1][0]
+    for i in n[1]:
+        if i > _max:
+            _max = i
+    _sum=0
+    for i in n[1]:
+        _sum+=i/_max*100
+    return _sum/n[0]
 
+print(solution())
+
+#enumerate 함수를 이용해보자

b603366d1ca0fa75e93dacb425476d158e3dd341
OneCrazyman <cjw7155@naver.com>
2023-08-10 16:57:34 +0900

    add 0810

---
+++ baekjoon/1837.py
@@ -0,0 +1,12 @@
+#암호제작
+num = [77,12]
+result = None
+for i in range(2,num[1]):
+    if num[0]%i==0:
+        result = "BAD %i" % i
+if result: print(result)
+else: print("GOOD")
+
+
+100g 80원
+100g 236원
--- baekjoon/8958.py
+++ baekjoon/8958.py
@@ -1,7 +1,22 @@
 #OX퀴즈
 a = []
+temp = []
+result=0
 n = int(input())
 for i in range(n):
     a.append(input())
 for _case in a:
-    
+    for i,value in enumerate(_case):
+        if value == "O":
+            temp.append(value)
+        else: #value == "X"
+            for _i, val in enumerate(temp):
+                _i+=1
+                result+=_i
+            temp  = []
+    for _i, val in enumerate(temp):
+        _i += 1
+        result += _i
+    temp = []
+    print(result)
+    result = 0

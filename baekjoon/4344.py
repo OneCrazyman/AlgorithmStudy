1061394036b9be72a20f9287ff2b4f257b4ca395
OneCrazyman <cjw7155@naver.com>
2023-08-11 14:59:57 +0900

    test

---
+++ baekjoon/4344.py
@@ -0,0 +1,18 @@
+#평균은넘겠지
+case_l =[]
+case_n = int(input())
+for i in range(case_n):
+    case_l.append(list(map(int,input().split())))
+
+for case in case_l:
+    sum=0
+    for i in range(1,case[0]+1):
+        sum+=case[i]
+    avr = sum/case[0] #평균
+    count=0
+    for i in range(1,case[0]+1):
+        if case[i] > avr:
+            count +=1 
+    result = count/case[0]*100
+
+    print("%3f")

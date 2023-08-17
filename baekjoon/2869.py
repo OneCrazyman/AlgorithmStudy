7950448fa784289a0dcb8a48160e28d1ffb4fdca
OneCrazyman <cjw7155@naver.com>
2023-08-17 16:29:02 +0900

    add 0810 mod: buymonster

--- baekjoon/2869.py
+++ baekjoon/2869.py
@@ -1,12 +1,18 @@
 #달팽이는 올라가고 싶다
 A,B,V = list(map(int,input().split()))
-now, count = 0, 0
 
 oneday = A-B
-print(V / oneday)
 
-5 1 20
-5
+lev = int(V / oneday)
 
-5 1 6
- 1 1.0
+for i in range(lev-1,-1,-1):
+    # print(i)
+    if i * oneday + A >= V:
+        continue
+    else:
+        result = i+2
+        break
+
+print(result)
+
+# 2 4 11

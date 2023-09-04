e77fdc5e03ce12bc5f08e14ea6cf605515d0166c
OneCrazyman <cjw7155@naver.com>
2023-09-04 20:22:03 +0900

    add. 1931

--- baekjoon/1931.py
+++ baekjoon/1931.py
@@ -1,9 +1,28 @@
 from sys import stdin as s
 
-s = open("input.txt","rt")
+s = open("input.txt", "rt")
 
 N = int(s.readline()) #회의수
-print(N)
+
+arr = []
 for _ in range(N):
     t1, t2 = map(int, s.readline().split())
-    print(t1, t2)
+    arr.append((t1, t2))
+arr.sort(key=lambda x: x[0])
+print(arr)
+
+mo = 0
+count = 0
+to = arr[0][0]
+for t1, t2 in arr:
+    mo -= t1-to
+    to = t1
+    m = t2 - t1
+    if m < mo:
+        mo = m; continue
+    if mo <= 0:
+        count += 1
+        print(t1, t2)
+        mo = m
+# print(f'답은 {count}')
+print(count)

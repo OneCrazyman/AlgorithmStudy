60b03b97ff686ac8dc29bd5c78dd7835901748c8
Choi Jaewon <98089603+OneCrazyman@users.noreply.github.com>
2024-03-12 16:25:54 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,19 +1,14 @@
 #https://www.acmicpc.net/problem/11047
 from sys import stdin as s
-# s = open("input.txt")
+#s = open("input.txt")
 N, K = map(int, s.readline().split())
 list = []
 result = 0
 for _ in range(N):
     list.append(int(s.readline()))
 #
-max = 0
-for n in list:
-    if n <= K:
-        if n > max:
-            max = n
 r = K
-for i in range(list.index(max),-1,-1):
+for i in range(N-1,-1,-1):
     result += r // list[i]
     r = K % list[i]
 #

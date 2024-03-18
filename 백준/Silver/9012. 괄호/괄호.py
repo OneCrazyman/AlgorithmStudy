512a0d57e5ca0505660743ff1934a2bfbb4c5138
Choi Jaewon <98089603+OneCrazyman@users.noreply.github.com>
2024-03-18 15:47:02 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,19 @@
+from sys import stdin as s
+#s = open("input.txt")
+N = int(s.readline())
+
+for _ in range(N):
+    stack = []
+    VPN = 'YES'
+    str = s.readline().strip()
+    for w in str:
+        if w == '(':
+            stack.append(w)
+        else:
+            try:
+                stack.pop()
+            except IndexError:
+                VPN = 'NO'
+    #
+    if stack: VPN = 'NO'
+    print(VPN)

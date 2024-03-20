dabb777c0d86b109fd74379704057e284c6342a4
Choi Jaewon <98089603+OneCrazyman@users.noreply.github.com>
2024-03-20 15:44:56 +0900

    [Bronze II] Title: 수 정렬하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,15 +1,6 @@
 from sys import stdin as s
 #s = open("input.txt")
 N = int(s.readline())
-min_num = 1000
 arr = [int(s.readline()) for i in range(N)]
-for i in range(N):
-    for j in range(i,N):
-        if min_num > arr[j]:
-            min_num = arr[j]
-    arr[arr.index(min_num)] = arr[i]
-    arr[i] = min_num
-    min_num = 1000
-answer = ""
-for i in arr:
-    print(i)
+arr.sort()
+print(*arr, sep="\n")

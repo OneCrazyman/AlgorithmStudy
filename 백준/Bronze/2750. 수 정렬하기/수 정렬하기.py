f6525ddbac9deb8e1819302feaa6e58dfe331672
Choi Jaewon <98089603+OneCrazyman@users.noreply.github.com>
2024-03-20 15:46:21 +0900

    [Bronze II] Title: 수 정렬하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,6 +1,4 @@
 from sys import stdin as s
 #s = open("input.txt")
 N = int(s.readline())
-arr = [int(s.readline()) for i in range(N)]
-arr.sort()
-print(*arr, sep="\n")
+print(*sorted([int(s.readline()) for i in range(N)]), sep="\n")

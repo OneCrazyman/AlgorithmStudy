dc4bb63600411b890fd6e055401928d9a286cb3d
Choi Jaewon <98089603+OneCrazyman@users.noreply.github.com>
2024-06-03 23:03:42 +0900

    [Silver IV] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1120. 문자열/문자열.cc
+++ 백준/Silver/1120. 문자열/문자열.cc
@@ -2,51 +2,32 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
-#include <sstream>
-
 using namespace std;
-string input;
 string a;
 string b;
 
 int main() {
 	//freopen("input.txt", "r", stdin);
 
-	getline(cin, input);
-	istringstream iss(input);
-
-	iss >> a >> b;
+	cin >> a >> b;
 
 	// 결과 출력
 	//std::cout << "First string: " << a << std::endl;
 	//std::cout << "Second string: " << b << std::endl;
 
-	int count = 0;
-	if (a.length() == b.length()) 
+	int count;
+	int ans = 0x3f3f3f3f;
+	for (int i = 0; i <= b.length() - a.length(); i++)
 	{
-		for (int i = 0; i < a.length(); i++)
-			if (a[i] != b[i]) 
+		count = 0;
+		for (int j = 0; j < a.length(); j++) {
+			//cout << a[j] << " " << b[i + j] << endl;
+			if (a[j] != b[i+j])
 				count++;
-	}
-	else
-	{
-		int diff_num = b.length() - a.length();
-		int min = 99999;
-		for (int i = 0; i <= diff_num; i++)
-		{
-			count = 0;
-			for (int j = 0; j < a.length(); j++) {
-				//cout << a[j] << " " << b[i + j] << endl;
-				if (a[j] != b[i+j])
-					count++;
-			}
-			if (min > count) {
-				min = count;
-			}
 		}
-		count = min;
+		ans = min(ans, count);
 	}
 	//answer
-	cout << count;
+	cout << ans;
 	return 0;
 }

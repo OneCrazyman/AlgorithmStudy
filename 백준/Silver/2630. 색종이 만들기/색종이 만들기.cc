61cf38bc9519876bace39f181f256a62a4eab691
Choi Jaewon <98089603+OneCrazyman@users.noreply.github.com>
2024-06-21 01:24:17 +0900

    [Silver II] Title: 색종이 만들기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -2,66 +2,49 @@
 #include <iostream>
 using namespace std;
 
-int N;
-int c0 = 0;
-int c1 = 0;
+int c0;
+int c1;
 
-int fun(int** array, int row, int col, int n) {
-	
+void fun(int** array, int row, int col, int n) {
 	if (n == 1)
 	{
 		if (array[row][col] == 0) c0++;
 		else c1++;
-		return 1;
+		return;
 	}
 	else
 	{
 		int tmp = array[row][col];
 		//int count = 0;
-		for (int i = row; i < row+n; i++)
-		{
-			for (int j = col; j < col+n; j++)
-			{
-				if (array[i][j] != tmp) {
-					return fun(array, row, col, n / 2)
-						+ fun(array, row + (n / 2), col, n / 2)
-						+ fun(array, row, col + (n / 2), n / 2)
-						+ fun(array, row + (n / 2), col + (n / 2), n / 2);
-				}
+		for (int i = row; i < row + n; i++) for (int j = col; j < col + n; j++) {
+			if (array[i][j] != tmp) {
+				fun(array, row, col, n / 2);
+				fun(array, row + (n / 2), col, n / 2);
+				fun(array, row, col + (n / 2), n / 2);
+				fun(array, row + (n / 2), col + (n / 2), n / 2);
+				return;
 			}
 		}
 		//전부 다 같다면
 		if (array[row][col] == 0) c0++;
 		else c1++;
-		return 1;
 	}
 }
 
 int main() {
 	//freopen("input.txt", "r", stdin);
-
+	int N;
 	cin >> N;
 	int** array = new int* [N];
+
 	for (int i = 0; i < N; i++)
 	{
 		array[i] = new int[N];
 	}
 
-	for (int i = 0; i < N; ++i)
-	{
-		for (int j = 0; j < N; ++j)
-		{
-			cin >> array[i][j];
-		}
+	for (int i = 0; i < N; ++i)	for (int j = 0; j < N; ++j) {
+		cin >> array[i][j];
 	}
-
-	// 배열 출력
-	/*for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
-			std::cout << array[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}*/
 
 	fun(array, 0, 0, N);
 

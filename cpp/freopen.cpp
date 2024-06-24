9db88e92011dfbfbd7363e764d7459c035c1782a
OneCrazyman <cjw7155@naver.com>
2024-06-24 17:48:56 +0900

    #

--- cpp/freopen.cpp
+++ cpp/freopen.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main() {
-	freopen("input.txt", "r", stdin);
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main() {
+//	freopen("input.txt", "r", stdin);
+//	return 0;
+//}

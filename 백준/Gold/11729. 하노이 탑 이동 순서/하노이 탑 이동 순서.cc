9b2d4e50c7bdd3750f532188842f3055d7895ad5
Choi Jaewon <98089603+OneCrazyman@users.noreply.github.com>
2024-07-10 03:37:15 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 140 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <ios>
+
+using namespace std;
+vector<pair<int, int>> vec;
+
+void TOH(int n, int a, int b, int c) {
+	if (n == 1) {
+		vec.push_back(make_pair(a, c));
+	}
+	else {
+		TOH(n - 1, a, c, b);
+		vec.push_back(make_pair(a, c));
+		TOH(n - 1, b, a, c);
+	}
+}
+
+void Print() {
+	cout << vec.size() << "\n";
+	for (const auto& pair : vec) {
+		cout << pair.first << " " << pair.second << "\n";
+	}
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+	//freopen("input.txt", "r", stdin);
+	int N;
+	cin >> N;
+	TOH(N,1,2,3);
+	Print();
+	return 0;
+}

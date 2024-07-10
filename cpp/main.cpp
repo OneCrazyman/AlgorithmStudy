f174c4a5c7a4b096da10391dde5cb1ac6aabed7b
OneCrazyman <cjw7155@naver.com>
2024-07-11 02:26:26 +0900

    #

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,32 +5,10 @@
 #include <ios>
 
 using namespace std;
-vector<pair<int, int>> vec;
-
-void TOH(int n, int a, int b, int c) {
-	if (n == 1) {
-		vec.push_back(make_pair(a, c));
-	}
-	else {
-		TOH(n - 1, a, c, b);
-		vec.push_back(make_pair(a, c));
-		TOH(n - 1, b, a, c);
-	}
-}
-
-void Print() {
-	cout << vec.size() << "\n";
-	for (const auto& pair : vec) {
-		cout << pair.first << " " << pair.second << "\n";
-	}
-}
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	freopen("input.txt", "r", stdin);
-	int N;
-	cin >> N;
-	TOH(N,1,2,3);
-	Print();
+	int a[5] = { 5,4,3,2,1 };
+	printf("%d", 2[a]);
+	printf("%d", *(a + 2));
 	return 0;
 }

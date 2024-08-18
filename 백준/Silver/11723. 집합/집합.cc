330ab2dcef866fc001e9ffe277ba4b3adacdc4fc
Choi Jaewon <98089603+OneCrazyman@users.noreply.github.com>
2024-08-18 17:25:16 +0900

    [Silver V] Title: 집합, Time: 560 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.cc
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+int BIT = 0;
+
+void add();
+void remove();
+void check();
+void toggle();
+void all();
+void empty();
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	//freopen("input.txt", "r", stdin);
+	
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		string s;
+		cin >> s;
+		/*if (s == "add") add();
+		if (s == "remove") remove();
+		if (s == "check") check();
+		if (s == "toggle") toggle();
+		if (s == "all") all();
+		if (s == "empty") empty();*/
+		if (s == "add") add();
+		else if (s == "remove") remove();
+		else if (s == "check") check();
+		else if (s == "toggle") toggle();
+		else if (s == "all") all();
+		else if (s == "empty") empty();
+	}
+	return 0;
+}
+
+void add() {
+	int i;
+	cin >> i;
+	BIT |= (1 << i);
+}
+void remove() {
+	int i;
+	cin >> i;
+	BIT &= ~(1 << i);
+}
+void check() {
+	int i;
+	cin >> i;
+	if (BIT & (1 << i)) cout << 1 << "\n";
+	else cout << 0 << "\n";
+}
+void toggle() {
+	int i;
+	cin >> i;
+	BIT ^= (1 << i);
+}
+void all() {
+	BIT = (1 << 21) - 1;
+}
+void empty() {
+	BIT = 0;
+}

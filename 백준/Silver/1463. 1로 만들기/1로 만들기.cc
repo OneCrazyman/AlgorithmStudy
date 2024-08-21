76b1beea9e4fb3fb68fd3baaccaddb098a70cb3e
Choi Jaewon <98089603+OneCrazyman@users.noreply.github.com>
2024-08-21 17:05:16 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+//int arr[10 * 6 + 1];
+int arr[1000001];
+int main() {
+	//freopen("input.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	arr[1] = 0;
+	arr[2] = 1;
+	arr[3] = 1;
+
+	int n;
+	cin >> n;
+
+	for (int i = 4; i <= n; i++)
+	{
+		arr[i] = arr[i - 1] + 1;
+
+		//2 로 나누어진다면, 그에 따른 최솟값 비교 
+		if (i % 2 == 0) {
+			arr[i] = ((arr[i/2] + 1) < arr[i]) ? arr[i/2] + 1 : arr[i];
+		}
+
+		//3 ""
+		if (i % 3 == 0) {
+			arr[i] = ((arr[i/3] + 1) < arr[i]) ? arr[i/3] + 1 : arr[i];
+		}
+	}
+	cout << arr[n];
+
+	return 0;
+}

1e8c5792a1efaae06739dc0354ca659d50dc4eba
Choi Jaewon <98089603+OneCrazyman@users.noreply.github.com>
2024-09-06 18:29:51 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,29 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 
-using namespace std;
-int T;
 
-int rec(int n) {
-	if (n == 0) {
-		return 1;
-	}
-	else if (n < 0) {
-		return 0;
-	}
-	return rec(n - 1) + rec(n - 2) + rec(n - 3);
-}
+using namespace std;
+
+int T;
+int dp[12] = { 0 };
 
 void sum_combination() {
 	int n;
 	cin >> n;
-	cout << rec(n) << "\n";
+	cout << dp[n] << "\n";
+}
+
+void calculate_dp() {
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+	
+	for (int i = 4; i < 11; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+	}
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	//freopen("input.txt", "r", stdin);
+
+	calculate_dp();
 	cin >> T;
 	for (int i = 0; i < T; i++)
 	{
